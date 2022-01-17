5c21ab4cafbfe88904a96016a687cff0411a5d20
Jamie Davidson <roastmastersp@gmail.com>
2022-01-17 18:39:07 +0000

    Consume client project in console project to prove it works

--- Source/Console/Program.cs
+++ Source/Console/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ChuckNorrisJokes.Client;
+
+var repository = new JokeRepository("https://api.chucknorris.io/");
+
+Console.WriteLine(repository.GetRandomJoke().Value);
+
+while (true)
+{
+    // TODO: Read individual key, j = new joke, p = previous joke, n = next joke in cache
+    Console.ReadKey();
+    Console.WriteLine(repository.GetRandomJoke().Value);
+}

3ee235b6af2b7dd94911fde18a611eb71c74e184
Jamie Davidson <roastmastersp@gmail.com>
2022-01-17 19:12:09 +0000

    Add printing of instructions to ConsoleWriter

--- Source/ConsoleProject/ConsoleWriter.cs
+++ Source/ConsoleProject/ConsoleWriter.cs
@@ -10,5 +10,9 @@ internal sealed class ConsoleWriter : IConsoleWriter
         Console.WriteLine($"Joke {currentIndex + 1} of {maxIndex}{Environment.NewLine}");
 
         Console.WriteLine($"{currentJoke.Value}{Environment.NewLine}");
+
+        Console.WriteLine($"j - Get a random joke{Environment.NewLine}" +
+                          $"p - Go back to previous joke{Environment.NewLine}" +
+                          $"n - Go to next joke{Environment.NewLine}");
     }
 }
